3539d37ed20c3fb83c48e171cc6cefb65f299c6f
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-12-26 19:41:13 +0100

    [fix] change a tag to Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="text-center grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -6,11 +8,11 @@ export default function Home() {
           <h1 className="text-3xl font-bold">Welcome to</h1>
           <h1 className="text-4xl font-bold text-primary">Recipe Explorer</h1>
         </div>
-        
-        <div className="flex flex-col items-center justify-center"> 
-          <a href="/login"  className="text-xl px-6 py-2 rounded text-secondry border-secondry border hover:bg-secondry hover:text-customText">
+
+        <div className="flex flex-col items-center justify-center">
+          <Link href="/api/auth/login" className="text-xl px-6 py-2 rounded text-secondry border-secondry border hover:bg-secondry hover:text-customText" >
             Start
-          </a>
+          </Link>
         </div>
       </main>
     </div>
